e6090b3d5c5562162be0896de39490b847731d15
nicomongeee <98415142+nicomongeee@users.noreply.github.com>
2024-01-24 13:40:21 -0600

    Unidad de Trabajo

---
+++ Northwind/DAL/Implementations/UnidadDeTrabajo.cs
@@ -0,0 +1,32 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class UnidadDeTrabajo : IUnidadDeTrabajo
+    {
+        private readonly NorthwindContext _context;
+        public bool Complete()
+        {
+            try
+            {
+                _context.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
