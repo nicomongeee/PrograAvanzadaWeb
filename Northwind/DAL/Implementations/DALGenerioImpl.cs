197d0d8776411eecc1717e1c45e741a8c4fff326
nicomongeee <98415142+nicomongeee@users.noreply.github.com>
2024-01-24 12:42:19 -0600

    DAL Generico

---
+++ Northwind/DAL/Implementations/DALGenerioImpl.cs
@@ -0,0 +1,78 @@
+﻿using DAL.Interfaces;
+using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Implementations
+{
+    public class DALGenerioImpl<TEntity> : IDALGenerico<TEntity> where TEntity : class
+    {
+
+        protected readonly NorthwindContext _Context;
+
+        public DALGenerioImpl(NorthwindContext northwindContext)
+        {
+            _Context = northwindContext;
+        }
+        public bool Add(TEntity entity)
+        {
+            try
+            {
+                _Context.Add(entity);
+                return true;
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+        }
+
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(int id)
+        {
+            return _Context.Set<TEntity>().Find(id);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _Context.Set<TEntity>().ToList();
+        }
+
+        public bool Remove(TEntity entity)
+        {
+            try
+            {
+                _Context.Set<TEntity>().Attach(entity);
+                _Context.Set<TEntity>().Remove(entity);
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Update(TEntity entity)
+        {
+            try
+            {
+                _Context.Entry(entity).State = EntityState.Modified;
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
