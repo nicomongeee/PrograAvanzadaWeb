ea40bf94dd8862a26c43ed2866accef3dd64e5d8
nicomongeee <98415142+nicomongeee@users.noreply.github.com>
2024-01-30 20:49:25 -0600

    Entidades Shippers

---
+++ Northwind/FrontEnd/ApiModels/Shippers.cs
@@ -0,0 +1,12 @@
+﻿namespace FrontEnd.ApiModels
+{
+    public class Shippers
+    {
+        public int ShipperId { get; set; }
+
+        public string CompanyName { get; set; } = null!;
+
+        public string? Phone { get; set; }
+
+    }
+}
---
+++ Northwind/FrontEnd/Models/ShippersViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FrontEnd.Models
+{
+    public class ShippersViewModel
+    {
+        public int ShipperId { get; set; }
+
+        public string CompanyName { get; set; } = null!;
+
+        public string? Phone { get; set; }
+
+    }
+}
