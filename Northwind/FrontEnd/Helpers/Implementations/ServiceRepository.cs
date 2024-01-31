ff62a1f4a2d0d910ad33bde19ea7db1bad6c2105
nicomongeee <98415142+nicomongeee@users.noreply.github.com>
2024-01-30 21:15:18 -0600

    Services Y url en AppSettings

---
+++ Northwind/FrontEnd/Helpers/Implementations/ServiceRepository.cs
@@ -0,0 +1,47 @@
+﻿using FrontEnd.Helpers.Interfaces;
+using System.Reflection;
+
+namespace FrontEnd.Helpers.Implementations
+{
+    public class ServiceRepository : IServiceRepository
+    {
+        public HttpClient Client { get; set; }
+
+        public ServiceRepository(HttpClient _client, IConfiguration configuration) 
+        {
+            Client = _client;
+            string baseurl = configuration.GetValue<string>("BackEnd:url");
+
+            Client.BaseAddress = new Uri(baseurl);
+       
+        }
+
+        public HttpResponseMessage GetHttpResponse(string url)
+        {
+            return  Client.GetAsync(url).Result;
+        }
+        public HttpResponseMessage PutResponse(string url, object model)
+        {
+            return Client.PutAsJsonAsync(url, model).Result;
+        }
+
+        public HttpResponseMessage PostResponse(string url, object model)
+        {
+            return Client.PutAsJsonAsync(url, model).Result;
+        }
+        public HttpResponseMessage DeleteResponse(string url)
+        {
+            return Client.DeleteAsync(url).Result;
+        }
+
+        public HttpResponseMessage GetResponse(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public HttpResponseMessage GetResponse(string url, object model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Northwind/FrontEnd/Helpers/Interfaces/IServiceRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace FrontEnd.Helpers.Interfaces
+{
+    public interface IServiceRepository
+    {
+        HttpResponseMessage GetResponse(string url);
+        HttpResponseMessage GetResponse(string url, object model);
+        HttpResponseMessage PostResponse(string url, object model);
+        HttpResponseMessage DeleteResponse(string url);
+
+    }
+}
